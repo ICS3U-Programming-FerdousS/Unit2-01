fd501f6d656b79345b25b5f48094b7c0d41a7da7
Ubuntu <ubuntu@ip-172-31-46-166.ec2.internal>
2022-02-22 17:55:22 +0000

    adding code for area and perimeter of circle in Python

---
+++ area_per_circle.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python3
+# Created by: Ferdous Sediqi
+# Created on: Feb. 22, 2022
+# This program calculates and displays the area and perimeter of a
+# circle with radius 9 cm.
+
+
+import math
+
+
+def main():
+    print("For a circle that has a radius")
+    print("of 9 cm:")
+    print("")
+    print("Area = {} cm^2". format(math.pi*9**2))
+    print("Perimeter = {} cm". format(2*math.pi*9))
+
+
+if __name__ == "__main__":
+    main()
