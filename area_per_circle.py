44c127ea43f2abc327c7407db0300a6d7edf3034
Ubuntu <ubuntu@ip-172-31-46-166.ec2.internal>
2022-02-22 23:03:51 +0000

    updated the code

--- area_per_circle.py
+++ area_per_circle.py
@@ -10,10 +10,10 @@ import math
 
 def main():
     print("For a circle that has a radius")
-    print("of 9 cm:")
+    print("of 15mm:")
     print("")
-    print("Area = {} cm^2". format(math.pi*9**2))
-    print("Perimeter = {} cm". format(2*math.pi*9))
+    print("Area = {} mm^2". format(math.pi*15**2))
+    print("Perimeter = {} mm". format(2*math.pi*15))
 
 
 if __name__ == "__main__":
